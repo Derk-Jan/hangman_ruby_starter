ad3ebecabe27c8e0cadf088e7276243b8f4fca9d
derk <derk.vandervaart@gmail.com>
2017-03-26 22:09:58 +0200

    working prototype

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,12 +1,47 @@
 require File.expand_path("../random_word", __FILE__)
+require_relative "random_word.rb"
+#attr_accessor :random_word
 
 class Hangman
   def initialize
     @random_word = RandomWord.new
     @bad_guesses_left = 10
+    @play = true
   end
+
+
 
   def play!
     puts "Hi from lib/hangman.rb!"
+    puts "Do you wanna play?"
+    while @play == true
+      puts "Type Yes or No"
+      answer = gets.chomp.upcase
+      if answer == "YES"
+        puts "Let's play!"
+        @random_word.word_length
+        self.start_play
+      elsif answer == "NO"
+        puts "You don't know what you are missing!"
+        @play = false
+      else
+      puts "I'm not sure? What do you want"
+      end
+    end
   end
+
+
+  def start_play
+    if @play == true
+    #puts "Guess the word with #{@word_length} characters:"
+    @random_word.game_on
+    #puts "guess which word is: #{@guess_string}"
+    #  puts "#{@randomword}"
+    else
+      exit
+    end
+end
+
+
+
 end
--- lib/random_word.rb
+++ lib/random_word.rb
@@ -1,13 +1,108 @@
 class RandomWord
   attr_reader :word
+  attr_accessor :word
+  attr_accessor :characters
+  attr_accessor :word_length
+  attr_accessor :guess_string
+
 
   WORDS = %w(programming monkeybusiness rubyesque styleguide)
 
   def initialize
     @word = WORDS.sample
+    @word_letters = @word.split(//)
+    @guesses = []
+    @counter = 9
   end
 
-  def positions_for(characters: [])
-    # for you to implement :)
+  def word_length
+    @word_length = @word.size
+    puts "test word_length #{@word_length}"
+  end
+
+
+  def game_on
+    @guess_string = ("-" * @word_length)
+    puts " guess which word: #{@guess_string}"
+    puts "Guess the word with #{@word_length} characters:"
+    # puts "Test word = #{@word}"
+    while @counter > 0 || @win == false
+      print "Type please one character or ...if you got a hint: the whole word:"
+      self.guess_string
+      if @word_match == true
+        puts "You are trying the whole word? Let see!"
+        self.word_match
+      else
+        self.character_search
+      end
+      @counter  -= 1
+      if @win == true
+        break
+      elsif @check == true
+        self.positions_for
+      elsif @counter ==  0
+        puts "Aah big, big,  big dissapointment: You've lost!"
+      else
+        puts "Next time better, lets try again"
+      end
+    end
+  end
+
+  def guess_string
+    @word_match = false
+    @characters = gets.chomp
+    if @characters.size > 1
+      @word_match = true
+    end
+    @guesses << @characters
+  end
+
+  def word_match
+   if @word == @characters
+     puts "you guessed the name right!"
+     puts "Do you wanna play again?"
+     @win = true
+   else
+     puts "you guessed the name wrong. try again"
+   end
+ end
+
+
+
+  def character_search
+      string = @word
+      #check_string = (0 ... string.length).find_all { |i| string[i,1] == @character}
+      if @word_letters.include? @characters
+        @check = true
+        puts "Very good you guessed right"
+      else
+        @check = false
+        puts "Sorry, try it once more"
+      end
+  end
+
+
+
+
+  def positions_for
+    @win = true
+    display_string = ''
+    @word.chars.each do |char|
+      if @guesses.include?(char)
+        display_string << char
+      else
+        display_string << '-'
+        @win= false
+      end
+    end
+    if @win == true
+      puts "Yes it is: #{display_string}"
+      puts "you are a winner"
+      puts "Do you wanna play again?"
+    else
+      display_string
+      puts "test  positions for method #{display_string}"
+      puts " you've got #{@counter} times left to choose."
+    end
   end
 end
